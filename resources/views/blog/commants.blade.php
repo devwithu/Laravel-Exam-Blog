253e6aca1c4856c59439f50d3c3270746501b6ad
DaeJin Jeong <devwithu@naver.com>
2017-10-05 19:16:54 +0900

    init

---
+++ resources/views/blog/commants.blade.php
@@ -0,0 +1,106 @@
+<article class="post-comments">
+    <h3><i class="fa fa-comments"></i> 5 Comments</h3>
+
+    <div class="comment-body padding-10">
+        <ul class="comments-list">
+            <li class="comment-item">
+                <div class="comment-heading clearfix">
+                    <div class="comment-author-meta">
+                        <h4>John Doe <small>January 14, 2016</small></h4>
+                    </div>
+                </div>
+                <div class="comment-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                    <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                </div>
+            </li>
+
+            <li class="comment-item">
+                <div class="comment-heading clearfix">
+                    <div class="comment-author-meta">
+                        <h4>John Doe <small>January 14, 2016</small></h4>
+                    </div>
+                </div>
+                <div class="comment-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                    <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+                    <ul class="comments-list-children">
+                        <li class="comment-item">
+                            <div class="comment-heading clearfix">
+                                <div class="comment-author-meta">
+                                    <h4>John Doe <small>January 14, 2016</small></h4>
+                                </div>
+                            </div>
+                            <div class="comment-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                            </div>
+                        </li>
+
+                        <li class="comment-item">
+                            <div class="comment-heading clearfix">
+                                <div class="comment-author-meta">
+                                    <h4>John Doe <small>January 14, 2016</small></h4>
+                                </div>
+                            </div>
+                            <div class="comment-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+                                <ul class="comments-list-children">
+                                    <li class="comment-item">
+                                        <div class="comment-heading clearfix">
+                                            <div class="comment-author-meta">
+                                                <h4>John Doe <small>January 14, 2016</small></h4>
+                                            </div>
+                                        </div>
+                                        <div class="comment-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                            <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+    <div class="comment-footer padding-10">
+        <h3>Leave a comment</h3>
+        <form>
+            <div class="form-group required">
+                <label for="name">Name</label>
+                <input name="name" class="form-control" id="name" type="text">
+            </div>
+            <div class="form-group required">
+                <label for="email">Email</label>
+                <input name="email" class="form-control" id="email" type="text">
+            </div>
+            <div class="form-group">
+                <label for="website">Website</label>
+                <input name="website" class="form-control" id="website" type="text">
+            </div>
+            <div class="form-group required">
+                <label for="comment">Comment</label>
+                <textarea name="comment" class="form-control" id="comment" rows="6"></textarea>
+            </div>
+            <div class="clearfix">
+                <div class="pull-left">
+                    <button class="btn btn-lg btn-success" type="submit">Submit</button>
+                </div>
+                <div class="pull-right">
+                    <p class="text-muted">
+                        <span class="required">*</span>
+                        <em>Indicates required fields</em>
+                    </p>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</article>
