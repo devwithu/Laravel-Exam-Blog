fe68af6de800eeee2aa400f5bcf29e9ce2ccf92e
DaeJin Jeong <devwithu@naver.com>
2017-10-06 11:51:43 +0900

    applly Markdown

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
-
+use GrahamCampbell\Markdown\Facades\Markdown;
 class Post extends Model
 {
     protected $dates = ['published_at'];
@@ -42,5 +42,15 @@ class Post extends Model
     public function scopePublished($query)
     {
       return $query->where("published_at", "<=", Carbon::now());
+    }
+
+    public function getBodyHtmlAttribute($value)
+    {
+      return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL ;
+    }
+
+    public function getExcerptHtmlAttribute($value)
+    {
+      return $this->excerpt ? Markdown::convertToHtml(e($this->body)) : NULL ;
     }
 }
